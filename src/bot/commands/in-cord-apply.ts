fca1cf73cf9a4cd1255e8d1b2730935721ee0b5d
BuildTools <unconfigured@null.spigotmc.org>
2024-04-03 18:39:56 -0400

    removed checking the db

--- src/bot/commands/in-cord-apply.ts
+++ src/bot/commands/in-cord-apply.ts
@@ -1,11 +1,9 @@
 import { MessageFlags } from "discord-api-types/v10";
 import type { ChatInputInteraction, Command } from "disploy";
-import { prisma } from "../../server/db/client";
-import { generateLinkChallenge } from "../../server/lib/linking";
 
 const InDiscordApply: Command = {
   name: "apply",
-  description: "How to apply for SafeCloudSMP",
+  description: "Apply for SafeCloudSMP",
 
   async run(interaction: ChatInputInteraction) {
     interaction.reply({ content: "To apply for the SMP, join the server at safecloud.quest and follow the instructions given to link your account. If you need more help, create a ticket!", flags: MessageFlags.Ephemeral });
